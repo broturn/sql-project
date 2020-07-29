77439f56be6172f7c711ecabe4851f7019e007ac
Brodie Turner <64994857+broturn@users.noreply.github.com>
2020-07-29 13:41:58 -0500

    updated scripts

--- population_script/population_script.sql
+++ population_script/population_script.sql
@@ -1,121 +1,265 @@
 USE sql_project;
 
-INSERT INTO students(students_name, student_grade_level, students_course_id_one, students_course_id_two, students_course_id_three, students_course_id_four)
-VALUES ("Abe", "Fs", 1, 2, 3, 4 );
+INSERT INTO students(students_name, students_course_id_one, students_course_id_two, students_course_id_three, students_course_id_four)
+VALUES ("Abe", 1, 2, 3, 4 );
 
-INSERT INTO students(students_name, student_grade_level, students_course_id_one, students_course_id_two, students_course_id_three, students_course_id_four)
-VALUES ("Daniel", "Sr", 1, 2, 3, 4 );
+INSERT INTO students(students_name, students_course_id_one, students_course_id_two, students_course_id_three, students_course_id_four)
+VALUES ("Daniel", 1, 2, 3, 4 );
 
-INSERT INTO students(students_name, student_grade_level, students_course_id_one, students_course_id_two, students_course_id_three, students_course_id_four)
-VALUES ("Christine", "Jr", 1, 2, 3, 4 );
+INSERT INTO students(students_name, students_course_id_one, students_course_id_two, students_course_id_three, students_course_id_four)
+VALUES ("Christine", 1, 2, 3, 4 );
 
-INSERT INTO students(students_name, student_grade_level, students_course_id_one, students_course_id_two, students_course_id_three, students_course_id_four)
-VALUES ("Cindy", "Sp", 1, 2, 3, 4 );
+INSERT INTO students(students_name,students_course_id_one, students_course_id_two, students_course_id_three, students_course_id_four)
+VALUES ("Cindy", 1, 2, 3, 4 );
 
-INSERT INTO professors(professors_name, professors_course_id)
+INSERT INTO professors(professors_name, professors_courses_id)
 VALUES ("Turning", 1);
 
-INSERT INTO professors(professors_name, professors_course_id)
+INSERT INTO professors(professors_name, professors_courses_id)
 VALUES ("Hawking", 2);
 
-INSERT INTO professors(professors_name, professors_course_id)
+INSERT INTO professors(professors_name, professors_courses_id)
 VALUES ("Turner", 3);
 
-INSERT INTO professors(professors_name, professors_course_id)
+INSERT INTO professors(professors_name, professors_courses_id)
 VALUES ("Morrow", 4);
 
-INSERT INTO courses(course_title, courses_student_id, courses_professors_id)
+INSERT INTO courses(courses_title, courses_students_id, courses_professors_id)
 VALUES ("Typing", 4, 4);
 
-INSERT INTO courses(course_title, courses_student_id, courses_professors_id)
+INSERT INTO courses(courses_title, courses_students_id, courses_professors_id)
 VALUES ("Typing", 3, 4);
 
-INSERT INTO courses(course_title, courses_student_id, courses_professors_id)
+INSERT INTO courses(courses_title, courses_students_id, courses_professors_id)
 VALUES ("Typing", 2, 4);
 
-INSERT INTO courses(course_title, courses_student_id, courses_professors_id)
+INSERT INTO courses(courses_title, courses_students_id, courses_professors_id)
 VALUES ("Typing", 1,  4);
 
-INSERT INTO courses(course_title, courses_student_id, courses_professors_id)
+INSERT INTO courses(courses_title, courses_students_id, courses_professors_id)
 VALUES ("History", 4, 3);
 
-INSERT INTO courses(course_title, courses_student_id, courses_professors_id)
+INSERT INTO courses(courses_title, courses_students_id, courses_professors_id)
 VALUES ("History", 3, 3);
 
-INSERT INTO courses(course_title, courses_student_id, courses_professors_id)
+INSERT INTO courses(courses_title, courses_students_id, courses_professors_id)
 VALUES ("History", 2, 3);
 
-INSERT INTO courses(course_title, courses_student_id, courses_professors_id)
+INSERT INTO courses(courses_title, courses_students_id, courses_professors_id)
 VALUES ("History", 1, 3);
 
-INSERT INTO courses(course_title, courses_student_id, courses_professors_id)
+INSERT INTO courses(courses_title, courses_students_id, courses_professors_id)
 VALUES ("Science", 4, 2);
 
-INSERT INTO courses(course_title, courses_student_id, courses_professors_id)
+INSERT INTO courses(courses_title, courses_students_id, courses_professors_id)
 VALUES ("Science", 3, 2);
 
-INSERT INTO courses(course_title, courses_student_id, courses_professors_id)
+INSERT INTO courses(courses_title, courses_students_id, courses_professors_id)
 VALUES ("Science", 2, 2);
 
-INSERT INTO courses(course_title, courses_student_id, courses_professors_id)
+INSERT INTO courses(courses_title, courses_students_id, courses_professors_id)
 VALUES ("Science", 1, 2);
 
-INSERT INTO courses(course_title, courses_student_id, courses_professors_id)
+INSERT INTO courses(courses_title, courses_students_id, courses_professors_id)
 VALUES ("Computer Science", 4, 1);
 
-INSERT INTO courses(course_title, courses_student_id, courses_professors_id)
+INSERT INTO courses(courses_title, courses_students_id, courses_professors_id)
 VALUES ("Computer Science", 3, 1);
 
-INSERT INTO courses(course_title, courses_student_id, courses_professors_id)
+INSERT INTO courses(courses_title, courses_students_id, courses_professors_id)
 VALUES ("Computer Science", 2, 1);
 
-INSERT INTO courses(course_title, courses_student_id, courses_professors_id)
+INSERT INTO courses(courses_title, courses_students_id, courses_professors_id)
 VALUES ("Computer Science", 1, 1);
 
-INSERT INTO grades(grades_title, grades_points_earned, grades_course_id, grades_student_id, grades_professor_id )
-VALUES ("Test",98, 1, 1);
+INSERT INTO grades(grades_title, grades_points_earned, grades_courses_id, grades_students_id, grades_professors_id )
+VALUES ("Test",98, 1, 1, 1);
 
-INSERT INTO grades(grades_title, grades_points_earned, grades_course_id, grades_student_id, grades_professor_id )
-VALUES ("Quiz", 50, 1, 1);
+INSERT INTO grades(grades_title, grades_points_earned, grades_courses_id, grades_students_id, grades_professors_id )
+VALUES ("Quiz", 50, 1, 1, 1);
 
-INSERT INTO grades(grades_title, grades_points_earned, grades_course_id, grades_student_id, grades_professor_id )
-VALUES ("Essay", 150, 1, 1);
+INSERT INTO grades(grades_title, grades_points_earned, grades_courses_id, grades_students_id, grades_professors_id )
+VALUES ("Essay", 150, 1, 1, 1);
 
-INSERT INTO grades(grades_title, grades_points_earned, grades_course_id, grades_student_id, grades_professor_id )
-VALUES ("Project", 130, 1, 1);
+INSERT INTO grades(grades_title, grades_points_earned, grades_courses_id, grades_students_id, grades_professors_id )
+VALUES ("Project", 130, 1, 1, 1);
 
-INSERT INTO grades(grades_title, grades_points_earned, grades_course_id, grades_student_id, grades_professor_id )
-VALUES ("Test",60, 2, 2);
+INSERT INTO grades(grades_title, grades_points_earned, grades_courses_id, grades_students_id, grades_professors_id )
+VALUES ("Test",75, 2, 1, 2);
 
-INSERT INTO grades(grades_title, grades_points_earned, grades_course_id, grades_student_id, grades_professor_id )
-VALUES ("Quiz", 30, 2, 2);
+INSERT INTO grades(grades_title, grades_points_earned, grades_courses_id, grades_students_id, grades_professors_id )
+VALUES ("Quiz", 48, 2, 1, 2);
 
-INSERT INTO grades(grades_title, grades_points_earned, grades_course_id, grades_student_id, grades_professor_id )
-VALUES ("Essay", 120, 2, 2);
+INSERT INTO grades(grades_title, grades_points_earned, grades_courses_id, grades_students_id, grades_professors_id )
+VALUES ("Essay", 160, 2, 1, 2);
 
-INSERT INTO grades(grades_title, grades_points_earned, grades_course_id, grades_student_id, grades_professor_id )
-VALUES ("Project", 99, 2, 2);
+INSERT INTO grades(grades_title, grades_points_earned, grades_courses_id, grades_students_id, grades_professors_id )
+VALUES ("Project", 140, 2, 1, 2);
 
-INSERT INTO grades(grades_title, grades_points_earned, grades_course_id, grades_student_id, grades_professor_id )
-VALUES ("Test",75, 3, 3);
+INSERT INTO grades(grades_title, grades_points_earned, grades_courses_id, grades_students_id, grades_professors_id )
+VALUES ("Test",100, 3, 1, 3);
 
-INSERT INTO grades(grades_title, grades_points_earned, grades_course_id, grades_student_id, grades_professor_id )
-VALUES ("Quiz", 30, 3, 3);
+INSERT INTO grades(grades_title, grades_points_earned, grades_courses_id, grades_students_id, grades_professors_id )
+VALUES ("Quiz", 55, 3, 1, 3);
 
-INSERT INTO grades(grades_title, grades_points_earned, grades_course_id, grades_student_id, grades_professor_id )
-VALUES ("Essay", 120, 3, 3);
+INSERT INTO grades(grades_title, grades_points_earned, grades_courses_id, grades_students_id, grades_professors_id )
+VALUES ("Essay", 180, 3, 1, 3);
 
-INSERT INTO grades(grades_title, grades_points_earned, grades_course_id, grades_student_id, grades_professor_id )
-VALUES ("Project", 100, 3, 3);
+INSERT INTO grades(grades_title, grades_points_earned, grades_courses_id, grades_students_id, grades_professors_id )
+VALUES ("Project", 160, 3, 1, 3);
 
-INSERT INTO grades(grades_title, grades_points_earned, grades_course_id, grades_student_id, grades_professor_id )
-VALUES ("Test",60, 4, 4);
+INSERT INTO grades(grades_title, grades_points_earned, grades_courses_id, grades_students_id, grades_professors_id )
+VALUES ("Test",92, 4, 1, 4);
 
-INSERT INTO grades(grades_title, grades_points_earned, grades_course_id, grades_student_id, grades_professor_id )
-VALUES ("Quiz", 48, 4, 4);
+INSERT INTO grades(grades_title, grades_points_earned, grades_courses_id, grades_students_id, grades_professors_id )
+VALUES ("Quiz", 45, 4, 1, 4);
 
-INSERT INTO grades(grades_title, grades_points_earned, grades_course_id, grades_student_id, grades_professor_id )
-VALUES ("Essay", 123, 4, 4);
+INSERT INTO grades(grades_title, grades_points_earned, grades_courses_id, grades_students_id, grades_professors_id )
+VALUES ("Essay", 130, 4, 1, 4);
 
-INSERT INTO grades(grades_title, grades_points_earned, grades_course_id, grades_student_id, grades_professor_id )
-VALUES ("Project", 115, 4, 4);
+INSERT INTO grades(grades_title, grades_points_earned, grades_courses_id, grades_students_id, grades_professors_id )
+VALUES ("Project", 150, 4, 1, 4);
+
+INSERT INTO grades(grades_title, grades_points_earned, grades_courses_id, grades_students_id, grades_professors_id )
+VALUES ("Test",50, 1, 2, 1);
+
+INSERT INTO grades(grades_title, grades_points_earned, grades_courses_id, grades_students_id, grades_professors_id )
+VALUES ("Quiz", 30, 1, 2, 1);
+
+INSERT INTO grades(grades_title, grades_points_earned, grades_courses_id, grades_students_id, grades_professors_id )
+VALUES ("Essay", 150, 1, 2, 1);
+
+INSERT INTO grades(grades_title, grades_points_earned, grades_courses_id, grades_students_id, grades_professors_id )
+VALUES ("Project", 105, 1, 2, 1);
+
+INSERT INTO grades(grades_title, grades_points_earned, grades_courses_id, grades_students_id, grades_professors_id )
+VALUES ("Test",65, 2, 2, 2);
+
+INSERT INTO grades(grades_title, grades_points_earned, grades_courses_id, grades_students_id, grades_professors_id )
+VALUES ("Quiz", 30, 2, 2, 2);
+
+INSERT INTO grades(grades_title, grades_points_earned, grades_courses_id, grades_students_id, grades_professors_id )
+VALUES ("Essay", 98, 2, 2, 2);
+
+INSERT INTO grades(grades_title, grades_points_earned, grades_courses_id, grades_students_id, grades_professors_id )
+VALUES ("Project", 30, 2, 2, 2);
+
+INSERT INTO grades(grades_title, grades_points_earned, grades_courses_id, grades_students_id, grades_professors_id )
+VALUES ("Test",45, 3, 2, 3);
+
+INSERT INTO grades(grades_title, grades_points_earned, grades_courses_id, grades_students_id, grades_professors_id )
+VALUES ("Quiz", 20, 3, 2, 3);
+
+INSERT INTO grades(grades_title, grades_points_earned, grades_courses_id, grades_students_id, grades_professors_id )
+VALUES ("Essay", 50, 3, 2, 3);
+
+INSERT INTO grades(grades_title, grades_points_earned, grades_courses_id, grades_students_id, grades_professors_id )
+VALUES ("Project", 20, 3, 2, 3);
+
+INSERT INTO grades(grades_title, grades_points_earned, grades_courses_id, grades_students_id, grades_professors_id )
+VALUES ("Test",99, 4, 2, 4);
+
+INSERT INTO grades(grades_title, grades_points_earned, grades_courses_id, grades_students_id, grades_professors_id )
+VALUES ("Quiz", 40, 4, 2, 4);
+
+INSERT INTO grades(grades_title, grades_points_earned, grades_courses_id, grades_students_id, grades_professors_id )
+VALUES ("Essay", 120, 4, 2, 4);
+
+INSERT INTO grades(grades_title, grades_points_earned, grades_courses_id, grades_students_id, grades_professors_id )
+VALUES ("Project", 110, 4, 2, 4);
+
+INSERT INTO grades(grades_title, grades_points_earned, grades_courses_id, grades_students_id, grades_professors_id )
+VALUES ("Test",90, 1, 3, 1);
+
+INSERT INTO grades(grades_title, grades_points_earned, grades_courses_id, grades_students_id, grades_professors_id )
+VALUES ("Quiz", 35, 1, 3, 1);
+
+INSERT INTO grades(grades_title, grades_points_earned, grades_courses_id, grades_students_id, grades_professors_id )
+VALUES ("Essay", 110, 1, 3, 1);
+
+INSERT INTO grades(grades_title, grades_points_earned, grades_courses_id, grades_students_id, grades_professors_id )
+VALUES ("Project", 130, 1, 3, 1);
+
+INSERT INTO grades(grades_title, grades_points_earned, grades_courses_id, grades_students_id, grades_professors_id )
+VALUES ("Test",100, 2, 3, 2);
+
+INSERT INTO grades(grades_title, grades_points_earned, grades_courses_id, grades_students_id, grades_professors_id )
+VALUES ("Quiz", 35, 2, 3, 2);
+
+INSERT INTO grades(grades_title, grades_points_earned, grades_courses_id, grades_students_id, grades_professors_id )
+VALUES ("Essay", 140, 2, 3, 2);
+
+INSERT INTO grades(grades_title, grades_points_earned, grades_courses_id, grades_students_id, grades_professors_id )
+VALUES ("Project", 180, 2, 1, 2);
+
+INSERT INTO grades(grades_title, grades_points_earned, grades_courses_id, grades_students_id, grades_professors_id )
+VALUES ("Test",85, 3, 3, 3);
+
+INSERT INTO grades(grades_title, grades_points_earned, grades_courses_id, grades_students_id, grades_professors_id )
+VALUES ("Quiz", 50, 3, 3, 3);
+
+INSERT INTO grades(grades_title, grades_points_earned, grades_courses_id, grades_students_id, grades_professors_id )
+VALUES ("Essay", 148, 3, 3, 3);
+
+INSERT INTO grades(grades_title, grades_points_earned, grades_courses_id, grades_students_id, grades_professors_id )
+VALUES ("Project", 125, 3, 3, 3);
+
+INSERT INTO grades(grades_title, grades_points_earned, grades_courses_id, grades_students_id, grades_professors_id )
+VALUES ("Test",89, 4, 3, 4);
+
+INSERT INTO grades(grades_title, grades_points_earned, grades_courses_id, grades_students_id, grades_professors_id )
+VALUES ("Quiz", 35, 4, 3, 4);
+
+INSERT INTO grades(grades_title, grades_points_earned, grades_courses_id, grades_students_id, grades_professors_id )
+VALUES ("Essay", 148, 4, 3, 4);
+
+INSERT INTO grades(grades_title, grades_points_earned, grades_courses_id, grades_students_id, grades_professors_id )
+VALUES ("Project", 125, 4, 3, 4);
+
+INSERT INTO grades(grades_title, grades_points_earned, grades_courses_id, grades_students_id, grades_professors_id )
+VALUES ("Test",96, 1, 4, 1);
+
+INSERT INTO grades(grades_title, grades_points_earned, grades_courses_id, grades_students_id, grades_professors_id )
+VALUES ("Quiz", 32, 1, 4, 1);
+
+INSERT INTO grades(grades_title, grades_points_earned, grades_courses_id, grades_students_id, grades_professors_id )
+VALUES ("Essay", 142, 1, 4, 1);
+
+INSERT INTO grades(grades_title, grades_points_earned, grades_courses_id, grades_students_id, grades_professors_id )
+VALUES ("Project", 122, 1, 4, 1);
+
+INSERT INTO grades(grades_title, grades_points_earned, grades_courses_id, grades_students_id, grades_professors_id )
+VALUES ("Test",75, 2, 4, 2);
+
+INSERT INTO grades(grades_title, grades_points_earned, grades_courses_id, grades_students_id, grades_professors_id )
+VALUES ("Quiz", 40, 2, 4, 2);
+
+INSERT INTO grades(grades_title, grades_points_earned, grades_courses_id, grades_students_id, grades_professors_id )
+VALUES ("Essay", 115, 2, 4, 2);
+
+INSERT INTO grades(grades_title, grades_points_earned, grades_courses_id, grades_students_id, grades_professors_id )
+VALUES ("Project", 125, 2, 4, 2);
+
+INSERT INTO grades(grades_title, grades_points_earned, grades_courses_id, grades_students_id, grades_professors_id )
+VALUES ("Test",94, 3, 4, 3);
+
+INSERT INTO grades(grades_title, grades_points_earned, grades_courses_id, grades_students_id, grades_professors_id )
+VALUES ("Quiz", 49, 3, 4, 3);
+
+INSERT INTO grades(grades_title, grades_points_earned, grades_courses_id, grades_students_id, grades_professors_id )
+VALUES ("Essay", 149, 3, 4, 3);
+
+INSERT INTO grades(grades_title, grades_points_earned, grades_courses_id, grades_students_id, grades_professors_id )
+VALUES ("Project", 129, 3, 4, 3);
+
+INSERT INTO grades(grades_title, grades_points_earned, grades_courses_id, grades_students_id, grades_professors_id )
+VALUES ("Test",97, 4, 4, 4);
+
+INSERT INTO grades(grades_title, grades_points_earned, grades_courses_id, grades_students_id, grades_professors_id )
+VALUES ("Quiz", 38, 4, 4, 4);
+
+INSERT INTO grades(grades_title, grades_points_earned, grades_courses_id, grades_students_id, grades_professors_id )
+VALUES ("Essay", 160, 4, 4, 4);
+
+INSERT INTO grades(grades_title, grades_points_earned, grades_courses_id, grades_students_id, grades_professors_id )
+VALUES ("Project", 136, 4, 4, 4);
--- tables.sql
+++ tables.sql
@@ -1,65 +1,105 @@
 -- Table Creation Code
-CREATE TABLE `students` (
-  `students_id` int NOT NULL AUTO_INCREMENT,
-  `students_name` varchar(45) NOT NULL,
-  `students_grade_level` varchar(2) DEFAULT NULL,
-  PRIMARY KEY (`students_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+CREATE TABLE `sql_project`.`students` (
+  `students_id` INT NOT NULL AUTO_INCREMENT,
+  `students_name` VARCHAR(45) NOT NULL,
+`students_course_id_one` INT NOT NULL AFTER `students_name`,
+ `students_course_id_two` INT NOT NULL AFTER `students_course_id_one`,
+ `students_course_id_three` INT NOT NULL AFTER `students_course_id_two`,
+ `students_course_id_four` INT NOT NULL AFTER `students_course_
+  PRIMARY KEY (`students_id`));
 
 CREATE TABLE `sql_project`.`courses` (
-  `courses_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `courses_id` INT NOT NULL AUTO_INCREMENT,
   `courses_title` VARCHAR(45) NOT NULL,
-  `courses_students_id` INT NULL,
+  `courses_students_id` INT NOT NULL,
   `courses_professors_id` INT NOT NULL,
-  PRIMARY KEY (`courses_id`),
-  UNIQUE INDEX `courses_title_UNIQUE` (`courses_title` ASC) VISIBLE,
-  UNIQUE INDEX `courses_professors_id_UNIQUE` (`courses_professors_id` ASC) VISIBLE);
+  PRIMARY KEY (`courses_id`));
 
   CREATE TABLE `sql_project`.`professors` (
   `professors_id` INT NOT NULL AUTO_INCREMENT,
   `professors_name` VARCHAR(45) NOT NULL,
   `professors_courses_id` INT NOT NULL,
-  PRIMARY KEY (`professors_id`),
-  UNIQUE INDEX `professors_id_UNIQUE` (`professors_id` ASC) VISIBLE);
+  `professors_students_id` INT NOT NULL,
+  PRIMARY KEY (`professors_id`));
 
   CREATE TABLE `sql_project`.`grades` (
   `grades_id` INT NOT NULL AUTO_INCREMENT,
   `grades_title` VARCHAR(45) NOT NULL,
   `grades_points_earned` INT NOT NULL,
-  `grades_points_possible` INT NOT NULL,
-  `grades_student_id` INT NULL,
-  PRIMARY KEY (`grades_id`),
-  UNIQUE INDEX `grades_title_UNIQUE` (`grades_title` ASC) VISIBLE);
+  `grades_students_id` INT NOT NULL,
+  `grades_professors_id` INT NOT NULL,
+  `grades_courses_id` INT NOT NULL,
+  PRIMARY KEY (`grades_id`));
 
 --   Foreign Key Assignment code
-ALTER TABLE `sql_project`.`courses` 
+ALTER TABLE `sql_project`.`students` 
+ADD INDEX `students_course_id_one_idx` (`students_course_id_one` ASC) VISIBLE,
+ADD INDEX `students_course_id_two_idx` (`students_course_id_two` ASC) VISIBLE,
+ADD INDEX `students_course_id_three_idx` (`students_course_id_three` ASC) VISIBLE,
+ADD INDEX `students_course_id_four_idx` (`students_course_id_four` ASC) VISIBLE;
+;
+ALTER TABLE `sql_project`.`students` 
+ADD CONSTRAINT `students_course_id_one`
+  FOREIGN KEY (`students_course_id_one`)
+  REFERENCES `sql_project`.`courses` (`courses_id`)
+  ON DELETE CASCADE
+  ON UPDATE NO ACTION,
+ADD CONSTRAINT `students_course_id_two`
+  FOREIGN KEY (`students_course_id_two`)
+  REFERENCES `sql_project`.`courses` (`courses_id`)
+  ON DELETE CASCADE
+  ON UPDATE NO ACTION,
+ADD CONSTRAINT `students_course_id_three`
+  FOREIGN KEY (`students_course_id_three`)
+  REFERENCES `sql_project`.`courses` (`courses_id`)
+  ON DELETE CASCADE
+  ON UPDATE NO ACTION,
+ADD CONSTRAINT `students_course_id_four`
+  FOREIGN KEY (`students_course_id_four`)
+  REFERENCES `sql_project`.`courses` (`courses_id`)
+  ON DELETE CASCADE
+  ON UPDATE NO ACTION;
+
+ ALTER TABLE `sql_project`.`courses` 
 ADD CONSTRAINT `courses_students_id`
   FOREIGN KEY (`courses_students_id`)
   REFERENCES `sql_project`.`students` (`students_id`)
-  ON DELETE CASCADE
+  ON DELETE NO ACTION
   ON UPDATE NO ACTION,
 ADD CONSTRAINT `courses_professors_id`
   FOREIGN KEY (`courses_professors_id`)
   REFERENCES `sql_project`.`professors` (`professors_id`)
-  ON DELETE CASCADE
-  ON UPDATE NO ACTION;
-
-  ALTER TABLE `sql_project`.`grades` 
-ADD INDEX `grades_student_id_idx` (`grades_student_id` ASC) VISIBLE;
-;
-ALTER TABLE `sql_project`.`grades` 
-ADD CONSTRAINT `grades_student_id`
-  FOREIGN KEY (`grades_student_id`)
-  REFERENCES `sql_project`.`students` (`students_id`)
   ON DELETE NO ACTION
   ON UPDATE NO ACTION;
-
+  
   ALTER TABLE `sql_project`.`professors` 
 ADD INDEX `professors_courses_id_idx` (`professors_courses_id` ASC) VISIBLE;
 ;
 ALTER TABLE `sql_project`.`professors` 
 ADD CONSTRAINT `professors_courses_id`
   FOREIGN KEY (`professors_courses_id`)
-  REFERENCES `sql_project`.`courses` (`courses_students_id`)
+  REFERENCES `sql_project`.`courses` (`courses_id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
+
+  ALTER TABLE `sql_project`.`grades` 
+ADD INDEX `grades_students_id_idx` (`grades_students_id` ASC) VISIBLE,
+ADD INDEX `grades_professors_id_idx` (`grades_professors_id` ASC) VISIBLE,
+ADD INDEX `grades_courses_id_idx` (`grades_courses_id` ASC) VISIBLE;
+;
+ALTER TABLE `sql_project`.`grades` 
+ADD CONSTRAINT `grades_students_id`
+  FOREIGN KEY (`grades_students_id`)
+  REFERENCES `sql_project`.`students` (`students_id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION,
+ADD CONSTRAINT `grades_professors_id`
+  FOREIGN KEY (`grades_professors_id`)
+  REFERENCES `sql_project`.`professors` (`professors_id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION,
+ADD CONSTRAINT `grades_courses_id`
+  FOREIGN KEY (`grades_courses_id`)
+  REFERENCES `sql_project`.`courses` (`courses_id`)
   ON DELETE NO ACTION
   ON UPDATE NO ACTION;
